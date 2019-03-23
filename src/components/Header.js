e75d6ca7fb1c2c150a59eb09253b2faefee8d9b8
Motasem Halawani <motasemhalawani@Motasems-MacBook-Pro.local>
2019-03-23 02:31:10 -0400

    fix typing order

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,40 +20,48 @@ const Texture = () => (
     <circle cx='50%' cy='90%' r='100%' fill='url(#headerPattern)' />
   </svg>
 );
-function Header(props) {
+class Header extends React.Component {
 
-  return (
-    <div className="header-wrapper">
-    <Texture>
-    </Texture>
-    <header className="header">
-      <div className="header__welcome">
-        <Typed
-            strings={['Hello!', 'Welcome to my page.', 'Motasem']} 
-            typeSpeed={70} 
-            backSpeed={40} 
+  constructor() {
+    super()
+    this.state = {
+      delay: 8000
+    }
+  }
+
+  render() {
+    return (
+      <div className="header-wrapper">
+      <Texture>
+      </Texture>
+      <header className="header">
+        <div className="header__welcome">
+          <Typed
+              strings={['Hello!', 'Welcome to my page.', 'Motasem']} 
+              typeSpeed={70} 
+              backSpeed={40} 
+              showCursor={false}
+              onComplete={() => { this.setState({delay: 0}) }}
+          />
+             <Typed
+            strings={[
+              'Developer',
+              'Entrepreneur',
+              'Online Marketer'
+            ]}
+            startDelay={this.state.delay}
+            typeSpeed={60}
+            backSpeed={80}
             showCursor={false}
-        />
+            attr="placeholder"
+            loop >
+            <input className="header__input" type="text"/>
+          </Typed>
+        </div>
+      </header>
       </div>
-      <div className="header__title">
-        <Typed
-          strings={[
-            'Developer',
-            'Entrepreneur',
-            'Online Marketer'
-          ]}
-          startDelay={8000}
-          typeSpeed={60}
-          backSpeed={80} 
-          attr="placeholder"
-          loop >
-          <input className="header__input" type="text"/>
-        </Typed>
-      </div>
-    </header>
-    </div>
-   
-  )
+    )
+  }
 }
 
 
