eac4a52246c1135422a95c0d5b5efcd0c5af583b
Motasem Halawani <mutasem.halawani@gmail.com>
2019-04-07 18:29:28 -0400

    FIX SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
       description: ""
     },
     plugins: [
+        `gatsby-plugin-react-helmet`,
         `gatsby-plugin-sass`,
         {
             resolve: `gatsby-plugin-typography`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React from 'react'
 import Header from '../components/Header/Header'
 import Headernav from '../components/Header/Headernav'
 import Main from '../components/Main/Main'
 import Footer from '../components/Footer'
+import { Helmet } from 'react-helmet'
 // TODO: replace font awesome for production
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faIgloo } from '@fortawesome/free-solid-svg-icons'
@@ -11,6 +12,13 @@ library.add(faIgloo)
 
 export default () => (
   <div id="intro" className="app">
+  <Helmet>
+    <meta charSet="utf-8" />
+    <meta name="description" content="Motasem Halawni Personal Website" />
+    <title>Motasem Halawni</title>
+    {/* TODO: SWITCH LINK LATER */}
+    <link rel="canonical" href="https://agitated-goldberg-3352d4.netlify.com/" />
+  </Helmet>
     <Headernav>
     </Headernav>
     <Header>
