e47b02308d514585238f3f5a1d7abeaba7ec4c63
mutasem <mutasem.halawani@gmail.com>
2019-04-14 18:29:06 -0400

    add header image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,13 +12,13 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header-wrapper">
-      <img alt="motasem-halawani-intro-background" className="header__img" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/>
+      <img alt="motasem-halawani-intro-background" className="header__img" src="../../../DSC03786.jpg"/>
       <header className="header container">
         <div className="header__welcome">
           <Typed
-            strings={['Motasem']} 
-            typeSpeed={70} 
-            backSpeed={40} 
+            strings={['Motasem']}
+            typeSpeed={70}
+            backSpeed={40}
             showCursor={false}
             onComplete={() => { this.setState({delay: 0}) }}
           />
