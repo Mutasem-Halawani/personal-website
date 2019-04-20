5f7e8d79fbf5a10cce7b2e02002670542fabdbcd
Motasem Halawani <mutasem.halawani@gmail.com>
2019-04-21 02:40:11 +0300

    add mobile styling and menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,32 +12,32 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header-wrapper">
-      <img alt="motasem-halawani-intro-background" className="header__img" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/>
-      <header className="header container">
-        <div className="header__welcome">
-          <Typed
-            strings={['Motasem']} 
-            typeSpeed={70} 
-            backSpeed={40} 
-            showCursor={false}
-            onComplete={() => { this.setState({delay: 0}) }}
-          />
-          <Typed
-            strings={[
-              'Developer',
-              'Entrepreneur',
-              'Online Marketer'
-            ]}
-            startDelay={this.state.delay}
-            typeSpeed={60}
-            backSpeed={80}
-            showCursor={false}
-            attr="placeholder"
-            loop >
-            <input className="header__input" type="text"/>
-          </Typed>
-        </div>
-      </header>
+        <img alt="motasem-halawani-intro-background" className="header__img" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" />
+        <header className="header container">
+          <div className="header__welcome">
+            <Typed
+              strings={['Motasem']}
+              typeSpeed={70}
+              backSpeed={40}
+              showCursor={false}
+              onComplete={() => { this.setState({ delay: 0 }) }}
+            />
+            <Typed
+              strings={[
+                'Developer',
+                'Entrepreneur',
+                'Online Marketer'
+              ]}
+              startDelay={this.state.delay}
+              typeSpeed={60}
+              backSpeed={80}
+              showCursor={false}
+              attr="placeholder"
+              loop >
+              <input className="header__input" type="text" />
+            </Typed>
+          </div>
+        </header>
       </div>
     )
   }
--- src/components/Header/Headernav.js
+++ src/components/Header/Headernav.js
@@ -1,31 +1,60 @@
 import React from "react"
 import Texture from '../Texture'
+import MediaQuery from 'react-responsive'
 
-function Headernav(props) {
-  return (
-		<div className="nav-container">
-			<Texture>
-			</Texture>
-			<nav className="nav header-nav container">
-				<a className="nav__link link logo-wrapper" href="#intro">
-					<h1>
-						M
-					</h1>
-				</a>
-				<div className="nav__wrapper">
-					<a className="nav__link box-effect link" href="#about">
-						About
-					</a>
-					<a className="nav__link box-effect link" href="#work">
-						Projects
-					</a>
-					<a className="nav__link box-effect link" href="#contact">
-						Contact
-					</a>
+class Headernav extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			isActive: false,
+			isMobile: window.innerWidth < 768
+		};
+
+		this.toggle = this.toggle.bind(this);
+	}
+
+	toggle() {
+		this.setState({ isActive: !this.state.isActive })
+	}
+
+	render() {
+		return (
+			<div>
+				<div className={`nav-container ${!this.state.isActive && this.state.isMobile ? 'd-none' : ''}`}>
+					<MediaQuery minWidth={769}>
+						<Texture>
+						</Texture>
+					</MediaQuery>
+					<nav className="nav header-nav container">
+						<a onClick={this.toggle} className="nav__link link logo-wrapper" href="#intro">
+							<h1>
+								M
+							</h1>
+						</a>
+						<div className="nav__wrapper">
+							<a onClick={this.toggle} className="nav__link box-effect link" href="#about">
+								About
+							</a>
+							<a onClick={this.toggle} className="nav__link box-effect link" href="#work">
+								Projects
+							</a>
+							<a onClick={this.toggle} className="nav__link box-effect link" href="#contact">
+								Contact
+							</a>
+						</div>
+					</nav>
 				</div>
-			</nav>
-		</div>
-  )
+				<MediaQuery maxWidth={768}>
+					<button onClick={this.toggle} className={`hamburger hamburger--squeeze ${this.state.isActive ? 'is-active hamburger' : ''}" type="button`}>
+						<span className="hamburger-box">
+							<span className="hamburger-inner"></span>
+						</span>
+					</button>
+				</MediaQuery >
+			</div >
+		)
+	}
 }
 
 export default Headernav
