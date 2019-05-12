ef3f6f3066af4a4bde07f0c5488f1415cc25511f
Motasem Halawani <mutasem.halawani@gmail.com>
2019-05-12 15:28:57 -0400

    add info for social sharing

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,13 @@ export default () => (
     <meta charSet="utf-8" />
     <meta name="description" content="Motasem Halawani Personal Website" />
     <title>Motasem Halawani</title>
-    <link rel="canonical" href="https://mtsm.io" />
+    <link rel="canonical" href="https://mtsm.io"/>
     <meta name="google-site-verification" content="jzmerGH9oJ26c3PpKqGDumkKPQ6vZRZLeDU449037Ko" />
+    <meta property="og:image" content="https://mtsm.io/static/DSC03786-06c874e5e98fa1f4cfbb05f5220b279c.jpg"/>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://mtsm.io"/>
+    <meta property="og:title" content="Motasem Halawani" />
+    <meta property="og:description" content="Motasem Halawani Personal Website"/>
   </Helmet>
     <Headernav>
     </Headernav>
